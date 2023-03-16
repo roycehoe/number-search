ec8c44ee38e643b13d6664996b7304e90df1d1d4
roycehoe <royceinbox@hotmail.com>
2023-03-16 13:38:04 +0800

    docs: Create doc strings for functions in PhoneNumberController, and the response format for the phone validation api

--- backend/controllers/PhoneNumberController.ts
+++ backend/controllers/PhoneNumberController.ts
@@ -6,7 +6,43 @@ dotenv.config();
 const { SECRET_KEY } = process.env;
 const PHONE_VALIDATION_BASE_URL = "https://phonevalidation.abstractapi.com/v1";
 
-export async function search(request: Request, response: Response) {
+export interface Format {
+  international: string;
+  local: string;
+}
+
+export interface Country {
+  code: string;
+  name: string;
+  prefix: string;
+}
+
+export interface PhoneValidationResponse {
+  phone: string;
+  valid: boolean;
+  format: Format;
+  country: Country;
+  location: string;
+  type: string;
+  carrier: string;
+}
+
+export async function save(request: Request, response: Response) {
+  // Saves phone data to phone database
+  const queryParams = request.query;
+  const phoneNumber = queryParams.q as string;
+  try {
+    const phoneNumberResponse = await axios.get(
+      `${PHONE_VALIDATION_BASE_URL}/?api_key=${SECRET_KEY}&phone=${phoneNumber}`
+    );
+    response.send(phoneNumberResponse.data as PhoneValidationResponse);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function get(request: Request, response: Response) {
+  // Retrieves phone data from phone database
   const queryParams = request.query;
   const phoneNumber = queryParams.q as string;
   try {
--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import * as PhoneNumberController from "./controllers/PhoneNumberController";
 const app = express();
 const port = 3000;
 
-app.get("/", PhoneNumberController.search);
+app.get("/", PhoneNumberController.save);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
