7b78c61478840406b1b3920bfeb73392b5098c23
roycehoe <royceinbox@hotmail.com>
2023-03-16 11:32:09 +0800

    feat: Integrate storage and retrieval of secret keys via .dotenv

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
+const { SECRET_KEY } = process.env;
 
 app.get("/", (request: Request, response: Response) => {
-  response.send("Hello World!");
+  response.send(`Hello World! Your secret key ${SECRET_KEY}`);
 });
 
 app.listen(port, () => {
