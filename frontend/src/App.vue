58efed4be1433e4a50d516632315875f54f13812
roycehoe <royceinbox@hotmail.com>
2023-03-17 09:22:40 +0800

    feat: Create barebone phone number input

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="bg-red-300">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div>
+    <div class="grid grid-rows-6 w-96">
+      <div class="phone-number-input bg-blue-800 row-span-1">
+        <div class="flex m-2">
+          <div>
+            <input type="text" placeholder="Phone number..." class="input input-bordered input-primary w-full h-full" />
+          </div>
+          <div class="ml-4">
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+      <div class="phone-number-result bg-green-800 row-span-5">
+        <p>PHONE NUMBER RESULT</p>
+      </div>
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
