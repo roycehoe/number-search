e91cea006a29a40d766fc3f813245811e51a69b0
roycehoe <royceinbox@hotmail.com>
2023-03-17 14:06:56 +0800

    feat: Create a reset button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -65,27 +65,35 @@ function handleSelectCountryCode(event) {
 <template>
   <div class="flex flex-col justify-center items-center">
     <h1 class="m-12">Find This Number</h1>
-    <select class="select w-full max-w-xs">
-      <option disabled selected>Select a country code</option>
-      <option v-for="{ country, countryCodes, isoCode2, isoCode3 } in codes">
-        <a @click="handleSelectCountryCode(countryCodes[0])">
-          {{ country }} +{{ countryCodes[0] }}
-        </a>
-      </option>
-    </select>
+    <div>
+      <select class="select w-full max-w-xs">
+        <option disabled selected>Select a country code</option>
+        <option v-for="{ country, countryCodes, isoCode2, isoCode3 } in codes">
+          <a @click="handleSelectCountryCode(countryCodes[0])">
+            {{ country }} +{{ countryCodes[0] }}
+          </a>
+        </option>
+      </select>
 
-    <div class="phone-number-input w-full max-w-xs">
-      <div>
-        <input v-model="searchInput" type="text" placeholder="eg. 5550199"
-          class="input input-bordered input-primary w-full mt-2" />
+      <div class="phone-number-input w-full max-w-xs">
+        <div>
+          <input v-model="searchInput" type="text" placeholder="eg. 5550199"
+            class="input input-bordered input-primary w-full mt-2" />
+        </div>
       </div>
+
+      <div class="flex mt-2 justify-end gap-2">
+        <button class="btn w-24" :class="{
+          'btn loading': isLoading
+        }" @click="handleClick">Reset</button>
+        <button class="btn w-24" :class="{
+          'btn loading': isLoading
+        }" @click="handleClick">Search</button>
+
+      </div>
+
     </div>
 
-    <div class="ml-4">
-      <button class="btn" :class="{
-        'btn loading': isLoading
-      }" @click="handleClick">Search</button>
-    </div>
     <div class="w-96">
       <Transition>
         <div v-if="!isLoading" class="phone-number-result bg-green-800 row-span-5 m-2">
