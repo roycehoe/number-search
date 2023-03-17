bd30215d58db7b3662a8fb18542ae3c5fa04a868
roycehoe <royceinbox@hotmail.com>
2023-03-17 09:56:33 +0800

    feat: Create skeleton for diplay of phone number results with mock data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,54 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+export interface Format {
+  international: string;
+  local: string;
+}
+
+export interface Country {
+  code: string;
+  name: string;
+  prefix: string;
+}
+
+export interface GetPhoneNumberResponse {
+  _id: string;
+  phone: string;
+  valid: boolean;
+  format: Format;
+  country: Country;
+  location: string;
+  type: string;
+  carrier: string;
+  createdAt: number;
+}
+
+
+const MOCK_DATA = {
+  "_id": "6413b4a4b372f80c7fe2916c",
+  "phone": "6591348138",
+  "valid": true,
+  "format": {
+    "international": "+6591348138",
+    "local": "9134 8138"
+  },
+  "country": {
+    "code": "SG",
+    "name": "Singapore",
+    "prefix": "+65"
+  },
+  "location": "Singapore",
+  "type": "mobile",
+  "carrier": "SingTel",
+  "createdAt": 1679013028962
+}
+
 
 const searchInput = ref("")
+const isLoading = ref(false)
+const mockData = ref(MOCK_DATA)
+// const isLoading = ref(true)
 
 function handleClick() {
   console.log(searchInput.value)
@@ -25,8 +71,15 @@ function handleClick() {
           </div>
         </div>
       </div>
-      <div class="phone-number-result bg-green-800 row-span-5">
-        <p>PHONE NUMBER RESULT</p>
+      <div class="phone-number-result bg-green-800 row-span-5 m-2">
+        <div class="flex">
+          <p class="text-left mr-2">Status: </p>
+          <div class="badge badge-success h-full">
+            Valid
+          </div>
+        </div>
+        <p class="text-left">Location: {{ mockData.country.name }}, {{ mockData.location }}</p>
+        <p class="text-left">Carrier: {{ mockData.carrier }}</p>
       </div>
     </div>
 
